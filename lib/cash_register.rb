24720262dd35a81a2a337cb42912359fa96123db
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2020-01-09 15:39:53 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  def initialize(discount=0)
+    @total=0
+    @discount=discount
+  end
+  def add_item(title,price,quantity=1)
+    self.total+=price*quantity
+  end
+  def apply_discount
+    self.discount
+  end
+  # def total
+  #   @total
+  # end
+  # def total=(total)
+  #   @total=total
+  # end
+end
